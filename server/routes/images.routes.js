667c06fd2e689ef73a4123a3445e32e94511e776
Orlando <luisorlandogomezjimenez@gmail.com>
2021-09-12 22:01:58 -0500

    first commit

---
+++ server/routes/images.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/api/images/upload", async (req, res) => {
+  console.log(req.files);
+
+  return res.json("received");
+});
+
+router.get("/api/images", async (req, res) => {});
+
+router.get("/api/images/:id", async (req, res) => {});
+
+router.delete("/api/images/:id", async (req, res) => {});
+
+export default router;
