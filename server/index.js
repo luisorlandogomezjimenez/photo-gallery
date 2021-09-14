b811a26057effa967b12f0dae6d38801ed59b322
Orlando <luisorlandogomezjimenez@gmail.com>
2021-09-14 12:08:58 -0500

    aws sdk

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import fileUpload from "express-fileupload";
 import indexRoutes from "./routes/index.routes";
 import imagesRoutes from "./routes/images.routes";
 
+import config from "./config";
+
 const app = express();
 
 app.set("port", process.env.PORT || 4000);
--- server/routes/images.routes.js
+++ server/routes/images.routes.js
@@ -1,9 +1,34 @@
 import { Router } from "express";
+import AWS from "aws-sdk";
+import config from "../config";
 
 const router = Router();
 
+const spacesEndpoint = new AWS.Endpoint(config.Endpoint);
+
+const s3 = new AWS.S3({
+  endpoint: spacesEndpoint,
+});
+
 router.post("/api/images/upload", async (req, res) => {
-  console.log(req.files);
+  const { file } = req.files;
+  console.log(file);
+
+  try {
+    const uploadObject = await s3
+      .putObject({
+        ACL: "public-read",
+        Bucket: config.BucketName,
+        Body: file.data,
+        Key: file.name,
+      })
+      .promise();
+
+    console.log(uploadObject);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 
   return res.json("received");
 });
