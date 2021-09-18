56da58bf28dc0b420f07f4d1d80f594ee2b5ba0b
Orlando <luisorlandogomezjimenez@gmail.com>
2021-09-18 11:17:08 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { Route, Switch } from "react-router-dom";
 
-import "./App.css";
 import ImageDetail from "./pages/ImageDetail";
 import ImageForm from "./pages/ImageForm";
 import ImageGallery from "./pages/ImageGallery";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <Switch>
-      <Route path="/" component={ImageGallery} />
-    </Switch>
+    <div className="bg-dark text-light">
+      <Navbar />
+      <div className="container p-4">
+        <Switch>
+          <Route path="/" component={ImageGallery} exact />
+          <Route path="/upload" component={ImageForm} />
+          <Route path="/images/:id" component={ImageDetail} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
--- src/pages/ImageForm.js
+++ src/pages/ImageForm.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const ImageForm = () => {
-  return <div>ImageForm</div>;
+  const [file, setFile] = useState();
+  const [title, setTitle] = useState("");
+
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("file", file);
+    formData.append("title", title);
+
+    const res = await axios.post("/api/images/upload", formData);
+    console.log(res);
+  };
+
+  return (
+    <div className="col-md-4 offset-md-4">
+      <div className="card bg-dark text-light rounded-0 p-4">
+        <div className="card-body">
+          <h3>Upload an Image</h3>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="form-control bg-dark text-light my-3 rounded-0"
+              placeholder="Write a title for your image"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <input
+              type="file"
+              className="form-control bg-dark text-light my-3 rounded-0"
+              onChange={handleChange}
+              required
+            />
+            <div className="my-3">
+              <button className="btn btn-success rounded-0 w-100">
+                Upload
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImageForm;
